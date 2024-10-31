292f95c732dbfa7239a6b39fbff1dd328eb3a509
Neha Pudota <nehadis123@gmail.com>
2024-10-31 15:30:30 -0400

    Update MessageInterface.java

--- MessageInterface.java
+++ MessageInterface.java
@@ -2,10 +2,6 @@ import java.util.ArrayList;
 
 public interface MessageInterface {
     public void sendMessage(User sender, User receiver, String content, String messageFile);
-<<<<<<< HEAD
-    public void deleteMessage(User sender, User receiver, String content);
-=======
     public void deleteMessage(User sender, User receiver, String content, String messageFile);
->>>>>>> refs/remotes/origin/nehasbranch
     public ArrayList<String> retreiveMessages(User sender, User receiver, String messageFile);
 }
