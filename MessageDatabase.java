f8b404ff72eb5386434c8d57653a942ba8e32f9d
Anika Thapar <anikathapar@pal-nat186-80-101.itap.purdue.edu>
2024-10-31 15:04:53 -0400

    finished deleteMessage method

--- MessageDatabase.java
+++ MessageDatabase.java
@@ -8,7 +8,36 @@ public class MessageDatabase implements MessageInterface {
     }
 
 
-    public void deleteMessage(User sender, User receiver, String content) {
+    public void deleteMessage(User sender, User receiver, String content, String messageFile) {
+        File tempfile = new File("tempFile.txt");
+        File originalFile = new File(messageFile);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(originalFile));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(tempfile))){
+            String line = br.readLine();
+            while (line != null) {
+                String[] messageInfo = line.split(";", 3); // split line into sender, receiver, and content
+                // if the lines are different, write the line into the temp file
+                if (!(messageInfo[0].equals(sender.getUsername()) && 
+                    messageInfo[1].equals(receiver.getUsername()) && 
+                    messageInfo[2].equals(content))) {
+                        //write line to temp file
+                        bw.write(line);
+                        bw.newLine();
+                        //read next line
+                        line = br.readLine();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Delete the original file and rename the temp file to the original filename
+        if (originalFile.delete()) {
+            tempfile.renameTo(originalFile);
+        } else {
+            System.out.println("Could not delete the original file.");
+        }
 
     }
 
