985727b7fd92361e357b773c6ef2b1a700e413b1
Girafficorn <gengjiexie123@gmail.com>
2024-10-31 17:17:30 -0400

    Local Test for the User Class

--- RunLocalTest.java
+++ RunLocalTest.java
@@ -30,6 +30,8 @@ public class RunLocalTest {
     }
 
     public static class TestCase {
+
+        @Test(timeout = 1000)
         public void MessageDatabaseDeclarationTest() {
             Class<?> clazz;
             int modifiers;
@@ -55,6 +57,7 @@ public class RunLocalTest {
         }
     }
 
+    @Test(timeout = 1000)
     public void UserDatabaseDeclarationTest() {
         Class<?> clazz;
         int modifiers;
@@ -75,5 +78,52 @@ public class RunLocalTest {
                 Modifier.isAbstract(modifiers));
         Assert.assertEquals("Ensure that `UserDatabase` implements 1 interfaces!",
                 1, superinterfaces.length);
+    }
+
+    @Test
+    public void testUserConstructorAndMethod() {
+        //Tests the constructor exist
+        try {
+            Class<?> clazz = User.class;
+            Constructor<?> constructor = clazz.getConstructor(String.class, String.class, String.class, Boolean.class,
+                    ArrayList.class, ArrayList.class);
+        } catch (NoSuchMethodException e) {
+            Assert.fail(
+                    "User Constructor with 3 String, boolean, 2 ArrayList parameter does not exist or is not public!");
+        }
+
+        // Instantiate the User object with its constructor
+        ArrayList<String> friend = new ArrayList<String>();
+        friend.add("friend1");
+        friend.add("friend2");
+        friend.add("friend3");
+        ArrayList<String> blocked = new ArrayList<String>();
+        blocked.add("blocked1");
+        blocked.add("blocked2");
+        blocked.add("blocked3");
+        User user = new User("GetterTest", "Password", "PFP", true, friend, blocked);
+
+        //Tests for getter of User Class
+        Assert.assertEquals("Ensure getUsername returns correct value", "GetterTest", user.getUsername());
+        Assert.assertEquals("Ensure getPassword returns correct value", "Password", user.getPassword());
+        Assert.assertEquals("Ensure getRestrictMessages returns correct value", true, user.getRestrictMessages());
+        Assert.assertEquals("Ensure getFriends returns correct value", friend, user.getFriends());
+        Assert.assertEquals("Ensure getBlocked returns correct value", blocked, user.getBlocked());
+        Assert.assertEquals("Ensure getPfp returns correct value", "PFP", user.getPfp());
+
+        //Tests for setters of User class
+        user.setUsername("ChangedUser");
+        user.setPassword("ChangedPassword");
+        user.setRestrictMessages(false);
+        user.setPfp("ChangedPFP");
+        user.setFriends(new ArrayList<String>());
+        user.setBlocked(new ArrayList<String>());
+
+        Assert.assertEquals("Ensure setUsername changes the correct attribute", "ChangedUser", user.getUsername());
+        Assert.assertEquals("Ensure setUsername changes the correct attribute", "ChangedPassword", user.getPassword());
+        Assert.assertEquals("Ensure setUsername changes the correct attribute", false, user.getRestrictMessages());
+        Assert.assertEquals("Ensure setUsername changes the correct attribute", new ArrayList<String>(), user.getFriends());
+        Assert.assertEquals("Ensure setUsername changes the correct attribute", new ArrayList<String>(), user.getBlocked());
+        Assert.assertEquals("Ensure setUsername changes the correct attribute", "ChangedPFP", user.getPfp());
     }
 }
