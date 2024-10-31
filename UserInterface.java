57f5ed6623ad06acbd36228af826c812db905707
MithunMahesh <127631440+MithunMahesh@users.noreply.github.com>
2024-10-31 16:57:06 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,5 +1,5 @@
 public interface UserInterface {
-    public void createUser(String username, String password, boolean restrictMessage);
+    public void createUser(String username, String password, String pfp, boolean restrictMessage);
     public boolean login(String username, String password);
     public boolean blockUser(User user, User blockUser);
     public boolean friendUser(User user, User friendUser);
