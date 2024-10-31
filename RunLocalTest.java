6f3f1c0b3a2a0f42e0d6d390227e211bec56dcab
Girafficorn <gengjiexie123@gmail.com>
2024-10-31 15:33:00 -0400

    JUnit finally works for me now

--- RunLocalTest.java
+++ RunLocalTest.java
@@ -1,5 +1,38 @@
+import org.junit.Test;
+import org.junit.After;
+import java.lang.reflect.Field;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.rules.Timeout;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import javax.swing.*;
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import java.lang.reflect.InvocationTargetException;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
 public class RunLocalTest {
     public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestCase.class);
+        if (result.wasSuccessful()) {
+            System.out.println("Excellent - Test ran successfully");
+        } else {
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
+    }
+    
+
+    public static class TestCase {
         
     }
 }
