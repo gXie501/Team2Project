b51368510b05884337797b39f071325e4efa03ca
Girafficorn <gengjiexie123@gmail.com>
2024-11-02 16:03:11 -0400

    Changed location of readLine()

--- MessageDatabase.java
+++ MessageDatabase.java
@@ -1,9 +1,11 @@
 import java.io.*;
 import java.util.ArrayList;
+
 public class MessageDatabase implements MessageInterface {
 
     public void sendMessage(User sender, User receiver, String content, String messageFile) {
-        //if the content is an image, it will be stored as the image location (ex: "dog.txt")
+        // if the content is an image, it will be stored as the image location (ex:
+        // "dog.txt")
         try (PrintWriter pw = new PrintWriter(messageFile)) {
             pw.println(sender.getUsername() + ";" + receiver.getUsername() + ";" + content);
         } catch (IOException e) {
@@ -11,26 +13,25 @@ public class MessageDatabase implements MessageInterface {
         }
     }
 
-
     public void deleteMessage(User sender, User receiver, String content, String messageFile) {
         File tempfile = new File("tempFile.txt");
         File originalFile = new File(messageFile);
 
         try (BufferedReader br = new BufferedReader(new FileReader(originalFile));
-             BufferedWriter bw = new BufferedWriter(new FileWriter(tempfile))){
+                BufferedWriter bw = new BufferedWriter(new FileWriter(tempfile))) {
             String line = br.readLine();
             while (line != null) {
                 String[] messageInfo = line.split(";", 3); // split line into sender, receiver, and content
                 // if the lines are different, write the line into the temp file
-                if (!(messageInfo[0].equals(sender.getUsername()) && 
-                    messageInfo[1].equals(receiver.getUsername()) && 
-                    messageInfo[2].equals(content))) {
-                        //write line to temp file
-                        bw.write(line);
-                        bw.newLine();
-                        //read next line
-                        line = br.readLine();
+                if (!(messageInfo[0].equals(sender.getUsername()) &&
+                        messageInfo[1].equals(receiver.getUsername()) &&
+                        messageInfo[2].equals(content))) {
+                    // write line to temp file
+                    bw.write(line);
+                    bw.newLine();
                 }
+                // read next line
+                line = br.readLine();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -49,7 +50,7 @@ public class MessageDatabase implements MessageInterface {
         ArrayList<String> messages = new ArrayList<>();
         String userOneUsername = user1.getUsername();
         String userTwoUsername = user2.getUsername();
-        
+
         try (BufferedReader bfr = new BufferedReader(new FileReader(messageFile))) {
             String line = bfr.readLine();
             while (line != null) {
@@ -58,13 +59,13 @@ public class MessageDatabase implements MessageInterface {
                 String newMessage = currentMessage.substring(currentMessage.indexOf(";") + 1);
                 String secondUser = newMessage.substring(0, newMessage.indexOf(";"));
 
-                if ((firstUser.equals(userOneUsername) || firstUser.equals(userTwoUsername)) && (secondUser.equals(userOneUsername) || secondUser.equals(userTwoUsername))) {
+                if ((firstUser.equals(userOneUsername) || firstUser.equals(userTwoUsername))
+                        && (secondUser.equals(userOneUsername) || secondUser.equals(userTwoUsername))) {
                     messages.add(currentMessage);
                 }
 
                 line = bfr.readLine();
-                
-                
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -72,5 +73,5 @@ public class MessageDatabase implements MessageInterface {
         return messages;
 
     }
-    
+
 }
