6addb16c50ef4c451eaed074112edb2ebc0ede80
Neha Pudota <nehadis123@gmail.com>
2024-11-03 17:00:57 -0500

    Comments and Javadoc for MessageDatabase and MessageInterface

--- MessageDatabase.java
+++ MessageDatabase.java
@@ -1,8 +1,22 @@
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * Team Project -- Message Database for Social Media App
+ * 
+ * File that sends, deletes, and retrieves messages from given users
+ * 
+ * @author Team 2, Lab 19
+ * 
+ * @version Nov. 3, 2024
+ * 
+ */
+
 public class MessageDatabase implements MessageInterface {
 
+
+    // Given two users, the sender, receiver, content, and messageFile in that order,
+    // sendMessage will add a message in the messageFile using the sender,receiver,content format
     public void sendMessage(User sender, User receiver, String content, String messageFile) {
         // if the content is an image, it will be stored as the image location (ex:
         // "dog.txt")
@@ -14,6 +28,9 @@ public class MessageDatabase implements MessageInterface {
         }
     }
 
+    // Given the sender, receiver, and the content of the message,
+    // deleteMessage will parse through the messageFile and delete the message with the specified
+    // sender, receiver, and content
     public void deleteMessage(User sender, User receiver, String content, String messageFile) {
         File tempfile = new File("tempFile.txt");
         File originalFile = new File(messageFile);
@@ -47,6 +64,7 @@ public class MessageDatabase implements MessageInterface {
 
     }
 
+    // given two users, retrieveMessages will retrieve all messages between those users in a given messageFiles
     public ArrayList<String> retrieveMessages(String userOneUsername, String userTwoUsername, String messageFile) {
         ArrayList<String> messages = new ArrayList<>();
        
