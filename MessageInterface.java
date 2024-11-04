a3e573d5894749fc867d42fb3183f34b88d6762c
MithunMahesh <127631440+MithunMahesh@users.noreply.github.com>
2024-11-03 19:05:25 -0500

    checkstyle

--- MessageInterface.java
+++ MessageInterface.java
@@ -1,20 +1,12 @@
 import java.util.ArrayList;
 
 /**
- * Team Project -- Message Interface for Social Media App
- * 
- * File that outlines the methods in the MessageDatabase class
- * 
- * @author Team 2, Lab 19
+ * Interface defining methods for handling messages in the MessageDatabase.
  * 
  * @version Nov. 3, 2024
- * 
  */
-
 public interface MessageInterface {
-    public void sendMessage(User sender, User receiver, String content, String messageFile);
-
-    public void deleteMessage(User sender, User receiver, String content, String messageFile);
-
-    public ArrayList<String> retrieveMessages(String userOneUsername, String userTwoUsername, String messageFile);
+    void sendMessage(User sender, User receiver, String content, String messageFile);
+    void deleteMessage(User sender, User receiver, String content, String messageFile);
+    ArrayList<String> retrieveMessages(String userOneUsername, String userTwoUsername, String messageFile);
 }
