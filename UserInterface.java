ad5c1c8f66ca60455d175d0106f53bdf647688f8
MithunMahesh <127631440+MithunMahesh@users.noreply.github.com>
2024-11-03 19:14:55 -0500

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,13 +1,19 @@
-import java.util.ArrayList;
-
 /**
- * Interface defining methods for handling user accounts in the UserDatabase.
+ * Team Project -- Run Local Test for Social Media App
+ * 
+ * Contains UserDatabase methods
+ * 
+ * @author Team 2, Lab 19
  * 
  * @version Nov. 3, 2024
+ * 
  */
 public interface UserInterface {
-    boolean createUser(String username, String password, String profilePicture, boolean restrictMessage);
-    boolean login(String username, String password);
-    boolean blockUser(User user, User blockUser);
-    boolean friendUser(User user, User friendUser);
+    public boolean createUser(String username, String password, String pfp, boolean restrictMessage);
+
+    public boolean login(String username, String password);
+
+    public boolean blockUser(User user, User blockUser);
+
+    public boolean friendUser(User user, User friendUser);
 }
