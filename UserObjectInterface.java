ce127022bd2a6149bed48a1747eea5744c3ff2d7
MithunMahesh <127631440+MithunMahesh@users.noreply.github.com>
2024-11-03 19:06:41 -0500

    checkstyle

--- UserObjectInterface.java
+++ UserObjectInterface.java
@@ -1,40 +1,22 @@
 import java.util.ArrayList;
 
 /**
- * Team Project -- Run Local Test for Social Media App
- * 
- * Contains User Object methods
- * 
- * @author Team 2, Lab 19
+ * Interface defining basic methods for a User object.
  * 
  * @version Nov. 3, 2024
- * 
  */
 public interface UserObjectInterface {
     String getUsername();
-
     String getPassword();
-
-    String getPfp();
-
+    String getProfilePicture();
     ArrayList<User> getBlocked();
-
     ArrayList<User> getFriends();
-
     Boolean getRestrictMessages();
-
     void setUsername(String username);
-
     void setPassword(String password);
-
-    void setPfp(String pfp);
-
+    void setProfilePicture(String profilePicture);
     void setBlocked(ArrayList<User> blocked);
-
     void setFriends(ArrayList<User> friends);
-
     void setRestrictMessages(Boolean restrictMessages);
-
     boolean equals(User user);
-
 }
